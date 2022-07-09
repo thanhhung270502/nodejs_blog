d37fbc25d6328f01e67b02ca8906e2c7bb04ef4d
thanhhung270502 <thanhhung270502@gmail.com>
2022-07-09 08:23:19 +0700

    [MVC] Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
@@ -32,27 +34,8 @@ app.engine(
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-    // res.send('Hello World!');
-});
-
-app.get('/news', (req, res) => {
-    console.log(req.query.q);
-    res.render('news');
-});
-
-app.get('/search', (req, res) => {
-    res.render('search');
-});
-
-app.post('/search', (req, res) => {
-    console.log(req.body);
-
-    res.send('');
-});
-
-// console.log(path.join(__dirname, 'resources\\views'));
+// routes init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
